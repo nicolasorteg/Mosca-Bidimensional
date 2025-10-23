bbca04147a279f68789b715642cd4b4a89504bef
nico <nicolasortegaf22@gmail.com>
2025-10-23 03:58:49 +0200

    declaro las constantes globales que se usarán

---
+++ Juego/Main.cs
@@ -0,0 +1,15 @@
+// empezamos creando el paquete Math que nos ayudará a generar la posición aleatoria de la Mosca.
+using Math;
+
+// opciones menú principal, declaras como const globales para mejorar la legibilidad y mantenibilidad del código
+const int OPCION_MENU_PRINCIPAL_JUGAR = 1;
+const int OPCION_MENU_PRINCIPAL_ESTADISTICAS = 2;
+const int OPCION_MENU_PRINCIPAL_COMO_JUGAR = 3;
+const int OPCION_MENU_PRINCIPAL_SALIR = 4;
+
+// opciones para elegir la dificultad del juego
+const int OPCION_MENU_JUEGO_FACIL = 1;
+const int OPCION_MENU_JUEGO_MEDIO = 2;
+const int OPCION_MENU_JUEGO_DIFICIL = 3;
+const int OPCION_MENU_JUEGO_MAESTRO = 4;
+const int OPCION_MENU_JUEGO_IMPOSIBLE = 5;
