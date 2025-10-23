59b26cf60fda22b2401d53db31ed69789467333c
nico <nicolasortegaf22@gmail.com>
2025-10-23 04:12:43 +0200

    acabo todas las constantes, de momento

--- Juego/Main.cs
+++ Juego/Main.cs
@@ -13,3 +13,40 @@ const int OPCION_MENU_JUEGO_MEDIO = 2;
 const int OPCION_MENU_JUEGO_DIFICIL = 3;
 const int OPCION_MENU_JUEGO_MAESTRO = 4;
 const int OPCION_MENU_JUEGO_IMPOSIBLE = 5;
+const int OPCION_MENU_JUEGO_SALIR = 6;
+
+// --------- tamaños de los paneles según dificultad ---------
+// FÁCIL
+const int FILAS_PANEL_FACIL = 6;
+const int COLUMNAS_PANEL_FACIL = 6;  
+// MEDIO
+const int FILAS_PANEL_MEDIO = 7;
+const int COLUMNAS_PANEL_MEDIO = 7;  
+// DIFÍCIL
+const int FILAS_PANEL_DIFICIL = 8;
+const int COLUMNAS_PANEL_DIFICIL = 8;  
+// MAESTRO
+const int FILAS_PANEL_MAESTRO = 10;
+const int COLUMNAS_PANEL_MAESTRO = 10;  
+// IMPOSIBLE
+const int FILAS_PANEL_IMPOSIBLE = 15;
+const int COLUMNAS_PANEL_IMPOSIBLE = 15;  
+
+// --------- vidas según dificultad ---------
+// FÁCIL
+const int VIDAS_FACIL = 1;
+// MEDIO
+const int VIDAS_MEDIO = 2;
+// DIFÍCIL
+const int VIDAS_DIFICIL = 2;
+// MAESTRO
+const int VIDAS_MAESTRO = 3;
+// IMPOSIBLE
+const int VIDAS_IMPOSIBLE = 5;
+
+// --------- probabilidad de defensa ---------
+const decimal PROBABILIDAD_PROTECCION_DIFICIL = 0.10; // 10%
+const decimal PROBABILIDAD_PROTECCION_MAESTRO = 0.15; // 15%
+const decimal PROBABILIDAD_PROTECCION_IMPOSIBLE = 0.50; // 50%
+
+
